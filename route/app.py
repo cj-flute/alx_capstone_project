78d5613e0366a16c6e141c6ec09f466922c4c5c5
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-10-24 22:46:17 +0100

    Budget tracker app

--- route/app.py
+++ route/app.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Budget Tracker App Server running!'
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
