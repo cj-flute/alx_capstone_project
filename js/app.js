7c80573f7bb7ba17c1f6df6a22d75c2cbbbf2442
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-10-24 21:41:55 +0100

    Budget tracker app

--- js/app.js
+++ js/app.js
@@ -1,52 +1,187 @@
+// Import of functions from utility.js
 import {
     addExpense,
     addIncome,
+    expenseData,
+    incomeData,
+    closeForm,
+    validate,
     removeExpense,
     removeIncome,
-    validateForm,
-    info,
-    closeForm,
-    expenseCancel
+    cancel,
+    renderCard,
+    balance
 } from "./utility.js";
 
-const expense = [];
-const addBtn = document.querySelector("#add-btn");
-addBtn.addEventListener('click', () => {
-    const isValidated = validateForm();
-    if (!isValidated) {
-        return;
-    }
-    const formInfo = info();
-    closeForm();
-    expense.push(formInfo)
-    console.log(expense);
-});
+const exOkBtn = document.querySelector("#ex-ok-btn");
+const inOkBtn = document.querySelector("#in-ok-btn");
+const exAddBtn = document.querySelector("#ex-add-btn");
+const inAddBtn = document.querySelector("#in-add-btn");
+const totalEx = document.querySelector("#total-ex");
+const totalIn = document.querySelector("#total-in");
+const budgetName = document.querySelector("#budget-name");
+const budgetAmt = document.querySelector("#budget-amount");
+const incomeName = document.querySelector("#income-name");
+const incomeAmt = document.querySelector("#income-amount");
 
 
 
-const btn = document.querySelectorAll(".btn");
+// For Expenditure
+const expense = []; // Array that holds the whole expense
 
-console.log(btn);
-
-btn[0].addEventListener('click', ()=>{
-    removeExpense();
-})
-
-btn[1].addEventListener('click', () => {
+// First add button to add an expense
+exOkBtn.addEventListener('click', () => {
     addExpense();
 });
 
-btn[2].addEventListener('click', () => {
-    removeIncome();
+// The Ok button that you triger After inputing the data of expense to be added
+exAddBtn.addEventListener('click', () => {
+    const isValidate = validate(budgetName, budgetAmt);
+    if (!isValidate) { 
+        return
+    }
+
+    const ex_renderer = document.querySelector('.expense-budget-div');
+    const exData = expenseData();
+    expense.push(exData);
+    renderCard(exData, ex_renderer, createExpenseCard);
+    console.log(expense);
+    let expenseTotalAmount = expense.reduce((accumulator, currentExpense) => {
+        return accumulator + parseInt(currentExpense.budget_amount);
+    }, 0);
+    totalEx.innerHTML = expenseTotalAmount;
+    closeForm();
 });
 
-btn[3].addEventListener('click', ()=>{
+
+// Function to create a new expense card
+const createExpenseCard = (expenseData) => {
+    const excardDiv = document.createElement("div");
+    excardDiv.setAttribute("class", "card");
+
+
+    const ex_budget_name = document.createElement("h3");
+    ex_budget_name.innerText = expenseData.budget_name;
+    excardDiv.appendChild(ex_budget_name);
+
+
+    const imgTag = document.createElement("img");
+    imgTag.setAttribute(
+        "src",
+        "/assets/images/pngtree-budget-line-icon-png-image_6620500.png"
+    );
+    imgTag.setAttribute("style", "width: 50px");
+    imgTag.setAttribute("style", "height: 50px");
+    imgTag.setAttribute("stlye", "border-radius: 50px");
+    excardDiv.appendChild(imgTag);
+
+
+    const currencyDiv = document.createElement("div");
+    currencyDiv.setAttribute("class", "currency");
+
+    const pTag = document.createElement("p");
+    const dollarSpanTag = document.createElement("span");
+    dollarSpanTag.innerText = "$";
+    pTag.appendChild(dollarSpanTag);
+    pTag.innerText = expenseData.budget_amount;
+
+    currencyDiv.appendChild(pTag);
+    excardDiv.appendChild(currencyDiv);
+
+    const removeCheckBox = document.createElement("input");
+    removeCheckBox.setAttribute("class", "remove");
+    removeCheckBox.setAttribute("type", "checkbox");
+    excardDiv.appendChild(removeCheckBox);
+
+    return excardDiv;    
+}
+
+
+
+// ################################
+// ################################
+// For Income
+const income = []; // Array that holds the whole income
+
+// First add button to add an income
+inOkBtn.addEventListener('click', () => {
     addIncome();
-})
+});
 
 
+// The ok button that you triger after inputing the data of income to be added
+inAddBtn.addEventListener('click', () => {
+    const isValidate = validate(incomeName, incomeAmt);
+    if (!isValidate) { 
+        return
+    }
+
+    const in_renderer = document.querySelector('.income-budget-div');
+    const inData = incomeData();
+    income.push(inData);
+    renderCard(inData, in_renderer, createIncomeCard);
+    console.log(income);
+    let incomeTotalAmount = income.reduce((accumulator, currentIncome) => {
+        return accumulator + parseInt(currentIncome.income_amount);
+    }, 0);
+    totalIn.innerHTML = incomeTotalAmount;
+    closeForm();
+});
+
+// Function to create a new income card
+const createIncomeCard = (incomeData) => {
+    const incardDiv = document.createElement("div");
+    incardDiv.setAttribute("class", "card");
+
+
+    const in_budget_name = document.createElement("h3");
+    in_budget_name.innerText = incomeData.income_name;
+    incardDiv.appendChild(in_budget_name);
+
+
+    const imgTag = document.createElement("img");
+    imgTag.setAttribute(
+        "src",
+        "/assets/images/pngtree-budget-line-icon-png-image_6620500.png"
+    );
+    imgTag.setAttribute("style", "width: 50px");
+    imgTag.setAttribute("style", "height: 50px");
+    imgTag.setAttribute("stlye", "border-radius: 50px");
+    incardDiv.appendChild(imgTag);
+
+
+    const currencyDiv = document.createElement("div");
+    currencyDiv.setAttribute("class", "currency");
+
+    const pTag = document.createElement("p");
+    const dollarSpanTag = document.createElement("span");
+    dollarSpanTag.innerText = "$";
+    pTag.appendChild(dollarSpanTag);
+    pTag.innerText = incomeData.income_amount;
+
+    currencyDiv.appendChild(pTag);
+    incardDiv.appendChild(currencyDiv);
+
+    const removeCheckBox = document.createElement("input");
+    removeCheckBox.setAttribute("class", "remove");
+    removeCheckBox.setAttribute("type", "checkbox");
+    incardDiv.appendChild(removeCheckBox);
+
+    return incardDiv;    
+}
+
+
+// #################################
+// #################################
+
+
+
+// The cancel button to pop ups
 const exCancel = document.querySelector("#ex-cancel");
+const inCancel = document.querySelector("#in-cancel");
 exCancel.addEventListener('click', () => {
-    expenseCancel();
+    cancel();
 })
-const settings = btn[8];
+inCancel.addEventListener('click', () => {
+    cancel();
+})
--- js/utility.js
+++ js/utility.js
@@ -1,68 +1,107 @@
+const budgetName = document.querySelector("#budget-name");
+const budgetAmt = document.querySelector("#budget-amount");
+const incomeName = document.querySelector("#income-name");
+const incomeAmt = document.querySelector("#income-amount");
+const expenseAddDiv = document.querySelector("#expense-add-div");
+const incomeAddDiv = document.querySelector("#income-add-div");
+
+
+
+// Function to make the div to add expense visible
 const addExpense = () => {
-    addDiv.style.display = "block";
-}
-const addIncome = () => { 
-    alert('add income');
+    expenseAddDiv.style.display = "block";
 }
 
+// Function to make the div to add income visible
+const addIncome = () => { 
+    incomeAddDiv.style.display = "block";
+}
+
+
+// Function to remove an expense
 const removeExpense = () => { 
     alert('remove expense');
 }
 
+// Function to remove an income
 const removeIncome = () => { 
     alert('remove income');
 }
-const budgetName = document.querySelector("#budget-name");
-const budgetAmt = document.querySelector("#budget-amount");
-const addDiv = document.querySelector(".add-div");
 
-const validateForm = () => { 
-    if (budgetName.value === '' && budgetAmt.value === '') {
-        budgetName.style.border = "1px solid red";
-        budgetAmt.style.border = "1px solid red";
-        return false;
+
+// Funtion that returns the value of the inputed Expense and ammount
+const expenseData = () => {
+    const budget_name = budgetName.value;
+    const budget_amount = budgetAmt.value;
+    return {
+        budget_name, budget_amount
     }
-    else if (budgetName.value === '') {
-        budgetName.style.border = "1px solid red";
-        return false;
+}
+
+// Fundtion that returns the value of the inputed income and ammount
+const incomeData = () => {
+    const income_name = incomeName.value;
+    const income_amount = incomeAmt.value;
+    return {
+        income_name, income_amount
     }
-    else if (budgetAmt.value === '') {
-        budgetAmt.style.border = "1px solid red";
+}
+
+// Funtion to reset the expense form or the income form
+const resetForm = () => {
+    budgetName.value = '';
+    budgetAmt.value = '';
+    incomeName.value = '';
+    incomeAmt.value = '';
+}
+
+// functioin that closes the inputed value 
+const closeForm = () => { 
+    expenseAddDiv.style.display = "none";
+    incomeAddDiv.style.display = "none";
+    resetForm();
+}
+
+// function to hide expense or income add imput
+const cancel = () => { 
+    expenseAddDiv.style.display = "none";
+    incomeAddDiv.style.display = "none";
+    resetForm();
+}
+
+// function to validate
+const validate = (name, amount) => { 
+    if (name.value === '' || amount.value === '') {
+        name.style.border = "1px solid red";
+        amount.style.border = "1px solid red";
         return false;
     }
     return true;
 }
-const info = () => { 
-    const budget_name = budgetName.value;
-    const budget_amount = budgetAmt.value;
-    return {
-        budget_name,budget_amount
-    }
+
+// Function to render the Expense or income card created
+const renderCard = (data, div, createFunc) => {
+    const item = createFunc(data);
+    div.appendChild(item);
 }
 
-const resetForm = () => {
-    budgetName.value = '';
-    budgetAmt.value = '';
-}
-const closeForm = () => { 
-    resetForm();
-    budgetAmt.style.border = '1px solid black';
-    budgetName.style.border = '1px solid black';
-    addDiv.style.display = "none";
-}
-const expenseCancel = () => { 
-    addDiv.style.display = "none";
+const balance = (incomeTotal, expenseTotal) => {
+    console.log(incomeTotal - expenseTotal);
+    return (incomeTotal - expenseTotal) 
 }
 
 
-
+// Funtions exported to app.js
 export {
     addExpense,
     addIncome,
     removeExpense,
     removeIncome,
-    validateForm,
-    info,
+    expenseData,
+    incomeData,
     closeForm,
-    expenseCancel
+    cancel,
+    validate,
+    renderCard,
+    balance
 }
